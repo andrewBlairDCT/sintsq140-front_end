334295edb2abff4de0590a8fd99c3b7c41773495
AndrewBlairDCT <andrew.blair@dcthomson.co.uk>
2024-12-06 15:14:09 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,107 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { Fragment, useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const Home = () => {
+  const [files, setFiles] = useState<File[]>([]);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const allowedFileTypes = ["text/csv", "application/vnd.ms-excel"]; 
+  const bucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
+
+  const s3 = new S3Client({
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY || '',
+    },
+  });
+
+  const handleUploadLocalFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (!e.target.files) return;
+
+    const selectedFiles = Array.from(e.target.files);
+    const filteredFiles = selectedFiles.filter((file) =>
+      allowedFileTypes.includes(file.type)
+    );
+    console.log('filtered', filteredFiles.length)
+    console.log('selected', selectedFiles.length)
+
+    if (filteredFiles.length !== selectedFiles.length) {
+      alert("Some file(s) are not the required type. Please choose files again.");
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
+      }
+    }
+    setFiles(filteredFiles);
+  };
+
+  const handleUploadS3 = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (files.length === 0) {
+      alert("Please select file(s) to upload.");
+      return;
+    }
+
+    try {
+      const uploadPromises = files.map(async (file) => {
+        const uniqueFileName = `${uuidv4()}-${file.name}`;
+        const uploadCommand = new PutObjectCommand({
+          Bucket: bucketName,
+          Key: uniqueFileName,
+          Body: file,
+        });
+
+        return s3.send(uploadCommand);
+      });
+
+      await Promise.all(uploadPromises);
+      alert("Files uploaded successfully!");
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
+      }
+      setFiles([]);
+    } catch (error) {
+      console.error("Error uploading files:", error);
+      alert(`Error uploading files: ${error.message}`);
+    }
+  };
+
+  const buttonStyling = {
+    zIndex: 12,
+    position: "relative",
+    border: "solid 1px white",
+    padding: 5,
+    backgroundColor: "grey",
+    margin: 5,
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <Fragment>
+      <div>
+        <div>
+          <p>Acceptable file format: .csv</p>
+          <div>
+            <input
+              type="file"
+              id="file-upload"
+              accept=".csv"
+              onChange={handleUploadLocalFile}
+              multiple
+              ref={fileInputRef}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          </div>
+          <div>
+            <button style={buttonStyling} onClick={handleUploadS3}>
+              S3 Upload
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </Fragment>
   );
-}
+};
+
+export default Home;
